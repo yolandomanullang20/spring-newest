14d27bf6e9b26bcd195b1e037f8856607de43fb1
Yolando Asri Erbenca Gegeh <yolando.erbenca@bankmandiri.co.id>
2023-12-07 16:38:54 +0700

    init(project)=> init project java 8

---
+++ src/main/java/com/yolandomanullang/cobaapi/CobaApiApplication.java
@@ -0,0 +1,13 @@
+package com.yolandomanullang.cobaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaApiApplication.class, args);
+	}
+
+}
